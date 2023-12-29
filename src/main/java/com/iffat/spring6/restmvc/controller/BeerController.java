f8e5843860026de9816bd847d8e0cc0ac3b823f8
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-30 05:38:36 +0700

    06. HTTP GET with Spring MVC List Operation

--- src/main/java/com/iffat/spring6/restmvc/controller/BeerController.java
+++ src/main/java/com/iffat/spring6/restmvc/controller/BeerController.java
@@ -4,16 +4,23 @@ import com.iffat.spring6.restmvc.model.Beer;
 import com.iffat.spring6.restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
 public class BeerController {
 
     private final BeerService beerService;
+
+    @RequestMapping("/api/v1/beer")
+    public List<Beer> listBeers() {
+        return beerService.listBeers();
+    }
 
     public Beer getBeerById(UUID id) {
         log.debug("Get Beer id - in controller");
--- src/main/java/com/iffat/spring6/restmvc/services/BeerService.java
+++ src/main/java/com/iffat/spring6/restmvc/services/BeerService.java
@@ -2,8 +2,10 @@ package com.iffat.spring6.restmvc.services;
 
 import com.iffat.spring6.restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+    List<Beer> listBeers();
     Beer getBeerById(UUID id);
 }
