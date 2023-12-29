a6b626a2e81272528d561f55d243f8a858a94baa
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-29 20:43:20 +0700

    05. Project Lombok Logging

--- src/main/java/com/iffat/spring6/restmvc/controller/BeerController.java
+++ src/main/java/com/iffat/spring6/restmvc/controller/BeerController.java
@@ -1,12 +1,22 @@
 package com.iffat.spring6.restmvc.controller;
 
+import com.iffat.spring6.restmvc.model.Beer;
 import com.iffat.spring6.restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 
+import java.util.UUID;
+
+@Slf4j
 @AllArgsConstructor
 @Controller
 public class BeerController {
 
     private final BeerService beerService;
+
+    public Beer getBeerById(UUID id) {
+        log.debug("Get Beer id - in controller");
+        return beerService.getBeerById(id);
+    }
 }
--- src/main/java/com/iffat/spring6/restmvc/services/BeerServiceImpl.java
+++ src/main/java/com/iffat/spring6/restmvc/services/BeerServiceImpl.java
@@ -2,14 +2,21 @@ package com.iffat.spring6.restmvc.services;
 
 import com.iffat.spring6.restmvc.model.Beer;
 import com.iffat.spring6.restmvc.model.BeerStyle;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Slf4j
+@Service
 public class BeerServiceImpl implements BeerService {
     @Override
     public Beer getBeerById(UUID id) {
+
+        log.debug("Get Beer id - in service");
+
         return Beer.builder()
                 .id(id)
                 .version(1)
