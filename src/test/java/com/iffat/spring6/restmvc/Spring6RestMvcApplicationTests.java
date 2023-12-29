2c968170948044f652382de2fe82c0d1bcea476c
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-29 19:21:11 +0700

    01. Starter Project

---
+++ src/test/java/com/iffat/spring6/restmvc/Spring6RestMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iffat.spring6.restmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6RestMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
