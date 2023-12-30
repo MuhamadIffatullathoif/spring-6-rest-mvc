9851b4f11198502d4f9715ec58e92c200fa4384a
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-30 17:29:53 +0700

    25. Controller Advice

--- src/main/java/com/iffat/spring6/restmvc/controller/BeerController.java
+++ src/main/java/com/iffat/spring6/restmvc/controller/BeerController.java
@@ -55,13 +55,6 @@ public class BeerController {
         return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
-    @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity handleNotFoundException() {
-        System.out.println("In exception handler");
-
-        return ResponseEntity.notFound().build();
-    }
-
     @GetMapping(BEER_PATH)
     public List<Beer> listBeers() {
         return beerService.listBeers();
