39390d96f735219f6d8041372c7ce2168dd60539
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-01 06:59:34 +0700

    74. [Query Param With Spring MVC] Find By Name with Spring Data JPA

--- src/test/java/com/iffat/spring6/restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/com/iffat/spring6/restmvc/repositories/BeerRepositoryTest.java
@@ -1,22 +1,34 @@
 package com.iffat.spring6.restmvc.repositories;
 
+import com.iffat.spring6.restmvc.bootstrap.BootstrapData;
 import com.iffat.spring6.restmvc.entities.Beer;
 import com.iffat.spring6.restmvc.model.BeerStyle;
+import com.iffat.spring6.restmvc.services.BeerCsvServiceImpl;
 import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
+@Import({BootstrapData.class, BeerCsvServiceImpl.class})
 class BeerRepositoryTest {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testGetBeerListByName() {
+        List<Beer> list = beerRepository.findAllByBeerNameIsLikeIgnoreCase("%IPA%");
+
+        assertThat(list.size()).isEqualTo(336);
+    }
 
     @Test
     void testSaveBeerNameTooLong() {
