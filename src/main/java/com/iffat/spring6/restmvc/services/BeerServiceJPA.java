b699862bd771629a6ed967c55e7e67f17e48cb29
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-31 07:39:51 +0700

    37. JPA Get Operations

--- src/main/java/com/iffat/spring6/restmvc/services/BeerServiceJPA.java
+++ src/main/java/com/iffat/spring6/restmvc/services/BeerServiceJPA.java
@@ -10,22 +10,27 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Primary
-public class BeerServiceJPA implements BeerService{
+public class BeerServiceJPA implements BeerService {
     private final BeerRepository beerRepository;
     private final BeerMapper beerMapper;
 
     @Override
     public List<BeerDTO> listBeers() {
-        return null;
+        return beerRepository.findAll()
+                .stream()
+                .map(beerMapper::beerToBeerDto).collect(Collectors.toList());
     }
 
     @Override
     public Optional<BeerDTO> getBeerById(UUID id) {
-        return Optional.empty();
+        return Optional
+                .ofNullable(beerMapper.beerToBeerDto(beerRepository
+                        .findById(id).orElse(null)));
     }
 
     @Override
