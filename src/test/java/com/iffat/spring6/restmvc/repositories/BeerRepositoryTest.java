7db699f8bc3ce1fda17cdcacc645cc034daf6c76
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-31 12:25:34 +0700

    50. JPA Validation

--- src/test/java/com/iffat/spring6/restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/com/iffat/spring6/restmvc/repositories/BeerRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.iffat.spring6.restmvc.repositories;
 
 import com.iffat.spring6.restmvc.entities.Beer;
+import com.iffat.spring6.restmvc.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,7 +20,12 @@ class BeerRepositoryTest {
     void testSaveBeers() {
         Beer savedBeer = beerRepository.save(Beer.builder()
                 .beerName("My Beer")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("232323")
+                .price(new BigDecimal("11.29"))
                 .build());
+
+        beerRepository.flush();
 
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
