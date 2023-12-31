9513105ac8d40a6e8b1cb629ef0da7b64d865494
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-31 11:28:09 +0700

    47. Controller Binding Validation

--- src/test/java/com/iffat/spring6/restmvc/controller/BeerControllerTest.java
+++ src/test/java/com/iffat/spring6/restmvc/controller/BeerControllerTest.java
@@ -1,5 +1,6 @@
 package com.iffat.spring6.restmvc.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.iffat.spring6.restmvc.model.BeerDTO;
 import com.iffat.spring6.restmvc.services.BeerService;
@@ -149,5 +150,18 @@ class BeerControllerTest {
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.id", is(testBeer.getId().toString())))
                 .andExpect(jsonPath("$.beerName", is(testBeer.getBeerName())));
+    }
+
+    @Test
+    void testCreateBeerNullBeerName() throws Exception {
+        BeerDTO beerDTO = BeerDTO.builder().build();
+
+        given(beerService.saveNewBeer(any(BeerDTO.class))).willReturn(beerServiceImpl.listBeers().get(1));
+
+        mockMvc.perform(post(BeerController.BEER_PATH)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(beerDTO)))
+                .andExpect(status().isBadRequest());
     }
 }
