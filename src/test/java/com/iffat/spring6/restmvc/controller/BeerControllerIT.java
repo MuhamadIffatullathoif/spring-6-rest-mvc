c256ed943ebb68a86d1893cdecf2f74d36d4802a
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-31 07:48:21 +0700

    38. Controller Integration Test

---
+++ src/test/java/com/iffat/spring6/restmvc/controller/BeerControllerIT.java
@@ -0,0 +1,39 @@
+package com.iffat.spring6.restmvc.controller;
+
+import com.iffat.spring6.restmvc.model.BeerDTO;
+import com.iffat.spring6.restmvc.repositories.BeerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BeerControllerIT {
+
+    @Autowired
+    BeerController beerController;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testListBeers() {
+        List<BeerDTO> beerDTOS = beerController.listBeers();
+        assertThat(beerDTOS.size()).isEqualTo(3);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testEmptyList() {
+        beerRepository.deleteAll();
+        List<BeerDTO> beerDTOS = beerController.listBeers();
+        assertThat(beerDTOS.size()).isEqualTo(0);
+    }
+}
